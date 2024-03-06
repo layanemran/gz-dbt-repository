af0233251ff6a74496d9535a9e7596f7ea8a5db4
layan hawsawi <layanemran2001@gmail.com>
2024-03-06 16:38:02 +0000

    finance_campaigns_day.sql Mart Model

--- models/mart/finance_campaigns_day.sql
+++ models/mart/finance_campaigns_day.sql
@@ -1,10 +1,10 @@
 SELECT 
   date_date,
   operational_margin - ads_cost AS ads_margin,
-  ROUND(average_basket,2) AS average_basket
+  ROUND(average_basket,2) AS average_basket,
   operational_margin,
   ads_cost,
-  ads_impressionn,
+  ads_impression,
   ads_clicks,
   quantity,
   revenue,
@@ -13,7 +13,7 @@ SELECT
   shipping_fee,
   logcost,
   ship_cost
-FROM {{ ref('int_campaigns_day') }}
-FULL OUTER JOIN {{ ref('finance_days') }} 
+FROM {{ref("int_campaigns_day")}}
+FULL OUTER JOIN {{ref("finance_days")}}
   USING (date_date)
 ORDER BY date_date DESC
